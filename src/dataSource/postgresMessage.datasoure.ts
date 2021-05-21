3829da8993c1495862f21224d67b38badb4cb626
jhoncbernal <jhoncbernal@gmail.com>
2021-05-21 11:11:03 -0500

    [MOD]:Implement new functions

--- src/dataSource/postgresMessage.datasoure.ts
+++ src/dataSource/postgresMessage.datasoure.ts
@@ -2,27 +2,45 @@ import PostgresMessageRepository from "../core/repositories/postgresMessage.repo
 import Message from "../core/entities/message";
 import pool from "../dbconfig/dbconnector";
 class PostgresMessage implements PostgresMessageRepository {
+  public async deleteDataTableMessages(): Promise<boolean> {
+    await this.executeQuery(`DELETE FROM messages RETURNING *;`);
+    return true;
+  }
 
-  public async sendMessage(fromUser:number,toUser:number,body:string): Promise<Message> {
-    const message: Message =await this.executeQuery(`INSERT INTO messages (from_user,to_user,body) VALUES ('${fromUser}','${toUser}','${body}') RETURNING *`);
+  public async sendMessage(
+    fromUser: number,
+    toUser: number,
+    body: string
+  ): Promise<Message> {
+    const message: Message = await this.executeQuery(
+      `INSERT INTO messages (from_user,to_user,body) VALUES ('${fromUser}','${toUser}','${body}') RETURNING *`
+    );
     return message;
   }
-  public async readMessagesByUsers(user1:number,user2:number): Promise<Message[]>{
-    const message: Message[] =await this.executeQuery(`SELECT * FROM messages 
+  public async readMessagesByUsers(
+    user1: number,
+    user2: number
+  ): Promise<Message[]> {
+    const message: Message[] = await this.executeQuery(`SELECT * FROM messages 
     WHERE (from_user = ${user1} OR from_user = ${user2}) AND (to_user = ${user1} OR to_user = ${user2}) ORDER BY date`);
     return message;
   }
-  private async executeQuery(query:string){
+  public async readMessagesByMessage(message: string): Promise<Message[]> {
+    const messages: Message[] = await this.executeQuery(`SELECT * FROM messages 
+    WHERE body = '${message}' ORDER BY date`);
+    return messages;
+  }
+  private async executeQuery(query: string) {
     const client = await pool.connect();
     return await new Promise<any>((resolve) => {
-        client
-          .query(query)
-          .then((result) => {
-            resolve(result.rows);
-          })
-          .catch((e) => console.error(e.stack))
-          .then(() => client.end());
-      });
+      client
+        .query(query)
+        .then((result) => {
+          resolve(result.rows);
+        })
+        .catch((e) => console.error(e.stack))
+        .then(() => client.end());
+    });
   }
 }
 export default PostgresMessage;
--- src/dataSource/postgresUser.datasource.ts
+++ src/dataSource/postgresUser.datasource.ts
@@ -2,30 +2,38 @@ import PostgresUserRepository from "../core/repositories/postgresUser.repository
 import User from "../core/entities/user";
 import pool from "../dbconfig/dbconnector";
 class PostgresUser implements PostgresUserRepository {
+  public async deleteDataTableUsers(): Promise<boolean> {
+    await this.executeQuery(`DELETE FROM users RETURNING *;`);
+    return true;
+  }
   public async getUsers(): Promise<User[]> {
-    const users: User[] =await this.executeQuery("SELECT * FROM users");
+    const users: User[] = await this.executeQuery("SELECT * FROM users");
     return users;
   }
-  public async getUserById(id:number): Promise<User> {
-    const user: User=await this.executeQuery(`SELECT * FROM users WHERE user_id=${id}`);
+  public async getUserById(id: number): Promise<User> {
+    const user: User = await this.executeQuery(
+      `SELECT * FROM users WHERE user_id=${id}`
+    );
     return user[0];
   }
-  public async insertUser(displayName:string,email:string): Promise<User> {
-    const users: User =await this.executeQuery(`INSERT INTO users (display_name,email) VALUES ('${displayName}','${email}') RETURNING *`);
+  public async insertUser(displayName: string, email: string): Promise<User> {
+    const users: User = await this.executeQuery(
+      `INSERT INTO users (display_name,email) VALUES ('${displayName}','${email}') RETURNING *`
+    );
     return users[0];
   }
-  
-  private async executeQuery(query:string){
+
+  private async executeQuery(query: string) {
     const client = await pool.connect();
     return await new Promise<any>((resolve) => {
-        client
-          .query(query)
-          .then((result) => {
-            resolve(result.rows);
-          })
-          .catch((e) => console.error(e.stack))
-          .then(() => client.end());
-      });
+      client
+        .query(query)
+        .then((result) => {
+          resolve(result.rows);
+        })
+        .catch((e) => console.error(e.stack))
+        .then(() => client.end());
+    });
   }
 }
 export default PostgresUser;
