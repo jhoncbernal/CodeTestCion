2e9a8fcc5c371dd83b98d8754e4f32acf10a13ee
jhoncbernal <jhoncbernal@gmail.com>
2021-05-21 11:10:37 -0500

    [MOD]:Add clean Table and read messages by message string

--- src/core/repositories/postgresMessage.repository.ts
+++ src/core/repositories/postgresMessage.repository.ts
@@ -3,5 +3,7 @@ import Message from '../entities/message';
 interface PostgresMessageRepository {
   sendMessage(fromUser:number,toUser:number,body:string): Promise<Message>;
   readMessagesByUsers(user1:number,user2:number): Promise<Message[]>;
+  readMessagesByMessage(message:string): Promise<Message[]>;
+  deleteDataTableMessages():Promise<boolean>;
 }
 export default PostgresMessageRepository;
