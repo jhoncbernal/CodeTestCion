48ca53af050bbea149226eacb0a3c8753005da7c
jhoncbernal <jhoncbernal@gmail.com>
2021-05-21 09:46:30 -0500

    [FIX]:add user  as a dinamic value

--- src/postgresConsole.ts
+++ src/postgresConsole.ts
@@ -6,32 +6,40 @@ import PostgresUser from "./dataSource/postgresUser.datasource";
 class PostgresConsole {
   private _postgresUser: PostgresUser;
   private _postgresMessage: PostgresMessage;
-
+  private user1: User;
+  private user2: User;
   constructor() {
     this._postgresUser = new PostgresUser();
     this._postgresMessage = new PostgresMessage();
   }
 
   private async insertUsers() {
-    await this._postgresUser.insertUser("Test 1", "test11@gmail.com");
-    await this._postgresUser.insertUser("Test 2", "test22@gmail.com");
+    this.user1 = await this._postgresUser.insertUser(
+      "Test 1",
+      `test${Math.random().toString()}@gmail.com`
+    );
+    this.user2 = await this._postgresUser.insertUser(
+      "Test 2",
+      `test2${Math.random().toString()}@gmail.com`
+    );
   }
 
   private async startConversation() {
-    await this._postgresMessage.sendMessage(1, 2, "Hey");
-    await this._postgresMessage.sendMessage(2, 1, "How are you Today?");
-    await this._postgresMessage.sendMessage(1, 2, "Awesome and you?");
-    await this._postgresMessage.sendMessage(2, 1, "Nothing new ");
+    await this._postgresMessage.sendMessage(this.user1.user_id, this.user2.user_id, "Hey");
+    await this._postgresMessage.sendMessage(this.user2.user_id, this.user1.user_id, "How are you Today?");
+    await this._postgresMessage.sendMessage(this.user1.user_id, this.user2.user_id, "Awesome and you?");
+    await this._postgresMessage.sendMessage(this.user2.user_id, this.user1.user_id, "Nothing new ");
   }
 
   private async consoleConversation() {
     const messages: Message[] = await this._postgresMessage.readMessagesByUsers(
-      1,
-      2
+      this.user1.user_id,
+      this.user2.user_id
     );
+    let users:User[]=[this.user1,this.user2];
     const chatConsole = await Promise.all(
       messages.map(async (message: Message) => {
-        const user:User = await this._postgresUser.getUserById(message.from_user);
+        const user: User =users.find((user:User) => user.user_id == message.from_user);
         return `${user.display_name}: ${message.body}`;
       })
     );
