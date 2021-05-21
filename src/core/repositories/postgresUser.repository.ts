7a567ad5d6a80aace9c01c9b0b4567e6ee933da5
jhoncbernal <jhoncbernal@gmail.com>
2021-05-21 11:09:59 -0500

    [MOD]:ADD CLEAN TABLE

--- src/core/repositories/postgresUser.repository.ts
+++ src/core/repositories/postgresUser.repository.ts
@@ -4,5 +4,6 @@ interface PostgresUserRepository {
   getUsers(): Promise<User[]>;
   getUserById(id:number): Promise<User>;
   insertUser(displayName:string,email:string): Promise<User>;
+  deleteDataTableUsers():Promise<boolean>;
 }
 export default PostgresUserRepository;
